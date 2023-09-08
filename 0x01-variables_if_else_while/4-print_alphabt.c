6742953fdeb8a39f9f527888b808f4483683985b
Sterling7s <ibrahimabubakarsiddiq4@gmail.com>
2023-09-08 16:06:16 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This program prints all programs except e and q
+ * Return: 0
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter = 'z'; letter++)
+putchar(letter);
+{
+if (letter != 'q'; &&letter != 'e')
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
