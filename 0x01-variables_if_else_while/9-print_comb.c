82901405f96d20a776ccbf79339d82ae3c38bc38
Sterling7s <ibrahimabubakarsiddiq4@gmail.com>
2023-09-09 07:03:47 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,14 @@ int main(void)
 
 	for (n = 0; n < 10; n++)
 	{
-		putchar(n + '0');
-		putchar(',');
-		putchar(' ');
+		if (i == 9)
+			putchar(n + '0');
+		else
+		{
+			putchar(n + '0');
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('\n');
 	return (0);
 }
