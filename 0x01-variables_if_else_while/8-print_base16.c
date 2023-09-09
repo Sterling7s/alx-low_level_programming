5a57a7212fce8f3ed2f342cff2f7f44137c688eb
Sterling7s <ibrahimabubakarsiddiq4@gmail.com>
2023-09-09 06:31:19 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This program prints both letters and numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	char i;
+
+	for (n = 0; n < 16; n++)
+		putchar(n);
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
