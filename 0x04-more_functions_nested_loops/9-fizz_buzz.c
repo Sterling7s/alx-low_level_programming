95358dc13b63da5dfb37eeceb4b8467770f660e2
Sterling7s <ibrahimabubakarsiddiq4@gmail.com>
2023-09-15 09:25:40 +0000

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,12 +11,15 @@ int main(void)
 	{
 		if ((num % 3) == 0 && (num % 5) == 0)
 			printf("Fizzbuzz");
+
 		else if ((num % 3) == 0)
 			printf("Fizz");
+
 		else if ((num % 5) == 0)
 			printf("Buzz");
 		else
 			printf("%d", num);
+
 		if (num == 100)
 			continue;
 		printf(" ");
